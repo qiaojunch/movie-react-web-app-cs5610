be070541ba0ff321813aa93e335cf56ef1d30963
xmeng <75476123+Mmona-meng@users.noreply.github.com>
2022-12-12 20:50:51 -0800

    profile

--- src/components/whoToExplore/WhoToExploreItem.jsx
+++ src/components/whoToExplore/WhoToExploreItem.jsx
@@ -6,17 +6,17 @@ const WhoToExploreItem = (
 ) => {
     return(
         <li className="list-group-item">
-            <div className="row">
-                <div className="col-2">
-                    <img className="rounded-circle" height={48} src={user.image} alt="icon"/>
+            <div>
+                <div>
+                    <img height={48} src={user.image} alt="icon"/>
                 </div>
                 <div className="info">
                     <h3>{user.userName}</h3>
                     <div>@{user._id}</div>
                     
                 </div>
-                <div className="col-2">
-                    <button className="button">Explore</button>
+                <div>
+                    <button>Explore</button>
                 </div>
             </div>
         </li>
